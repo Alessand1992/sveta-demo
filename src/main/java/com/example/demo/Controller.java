9505bab97ec15e3e401fc9e25ab83c395bd20e0e
Alessandless@gmail.com <Alessandless@gmail.com>
2023-08-05 22:03:03 +0600

    new

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/goodController")
+@RequestMapping("/api/Controller")
 @AllArgsConstructor
 @Slf4j
 public class Controller {
